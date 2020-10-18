94167a4b26517e483efc6cd41e1d1e886ca09869
Jonatan Koroch de Aguiar <jonatanaguiar1998@hotmail.com>
2020-10-18 18:23:16 -0300

    Finalizando projeto inicial

---
+++ app/src/main/java/br/com/koroch/agenda/dao/AlunoDAO.java
@@ -0,0 +1,17 @@
+package br.com.koroch.agenda.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.koroch.agenda.model.Aluno;
+
+public class AlunoDAO {
+    private final static List<Aluno> alunos = new ArrayList<>();
+    public void salva(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public List<Aluno> todos() {
+        return new ArrayList<>(alunos);
+    }
+}
--- app/src/main/java/br/com/koroch/agenda/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/koroch/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,46 +2,82 @@ package br.com.koroch.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.combateafraude.activefaceliveness.ActiveFaceLivenessActivity;
-import com.combateafraude.activefaceliveness.input.ActiveFaceLiveness;
-import com.combateafraude.activefaceliveness.output.ActiveFaceLivenessResult;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.com.koroch.agenda.R;
+import br.com.koroch.agenda.dao.AlunoDAO;
+import br.com.koroch.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULOAPPBAR = "Novo Aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    final AlunoDAO dao = new AlunoDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        setTitle(TITULOAPPBAR);
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
 
-        ActiveFaceLiveness mActiveFaceLiveness = new ActiveFaceLiveness.Builder("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc3ZWQ1Y2UxLTIzMTYtNDBkZi1iNzM3LTkzNmE2N2I3YjUwZCIsImlhdCI6MTU2NTk2MjMxM30.-oWGhwn54Mrt-aHuJVAFFBP4jY2AzXBPDQDePQZEQTw")
-                // veja a tabela abaixo
-                .build();
-        Intent mIntent = new Intent(this, ActiveFaceLivenessActivity.class);
-        mIntent.putExtra(ActiveFaceLiveness.PARAMETER_NAME, mActiveFaceLiveness);
-        startActivityForResult(mIntent, 1);
-
-
+//        ActiveFaceLiveness mActiveFaceLiveness = new ActiveFaceLiveness.Builder("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1ZjIzNDBhM2UxMjY5YjAwMDdlOTdmOGEifQ.pWN09KRGTPZdxHMHjjpgp-HvAvkyFmqkz6RRa7FX0T8")
+//                // veja a tabela abaixo
+//                .build();
+//        Intent mIntent = new Intent(this, ActiveFaceLivenessActivity.class);
+//        mIntent.putExtra(ActiveFaceLiveness.PARAMETER_NAME, mActiveFaceLiveness);
+//        startActivityForResult(mIntent, 1);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1){
-            if (resultCode == RESULT_OK && data != null) {
-                ActiveFaceLivenessResult mActiveFaceLivenessResult = (ActiveFaceLivenessResult) data.getSerializableExtra(ActiveFaceLivenessResult .PARAMETER_NAME);
-                if (mActiveFaceLivenessResult.wasSuccessful()){
-                    mActiveFaceLivenessResult.getImage();
-                }else{
-
-                }
-            } else {
-                // o usuário fechou a activity
+    private void configuraBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Aluno alunoNovo = criaAluno();
+                salvaAluno(alunoNovo);
             }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+        });
     }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salvaAluno(Aluno alunoNovo) {
+        dao.salva(alunoNovo);
+        finish();
+    }
+
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+        return new Aluno(nome, telefone, email);
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == 1){
+//            if (resultCode == RESULT_OK && data != null) {
+//                ActiveFaceLivenessResult mActiveFaceLivenessResult = (ActiveFaceLivenessResult) data.getSerializableExtra(ActiveFaceLivenessResult .PARAMETER_NAME);
+//                if (mActiveFaceLivenessResult.wasSuccessful()){
+//                    mActiveFaceLivenessResult.getImage();
+//                }else{
+//
+//                }
+//            } else {
+//                // o usuário fechou a activity
+//            }
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 }
